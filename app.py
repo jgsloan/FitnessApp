fded8a4c79b5c52f5a056f3e7fe85feac4d5e37f
John <jg.sloan10@gmail.com>
2024-02-18 22:04:27 +0000

    Updated login screen and added a create account page

--- app.py
+++ app.py
@@ -3,10 +3,26 @@ import os
 from flask import Flask, flash, redirect, render_template, request, sessions
 from flask_session import Session
 
+from helpers import login_required
+
 # Configure application
 app = Flask(__name__)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
+@login_required
 def index():
     return render_template("login.html")
 
@@ -16,3 +32,7 @@ def login():
         return render_template("login.html")
     else:
         return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
